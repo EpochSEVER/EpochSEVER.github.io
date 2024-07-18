a3915d7fd8d4fec79459df1dd3bb8ce5ee8e870d
EpochSEVER <iis20160512@gmail.com>
2024-07-19 00:56:55 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,1 @@
-document.addEventListener('mousemove', function(e) {
-    var circle = document.getElementById('circle');
-    circle.style.left = (e.pageX - 50) + 'px';
-    circle.style.top = (e.pageY - 50) + 'px';
-});
+
